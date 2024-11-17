0d502eeafaf20f0401f6d497b4f4f217c99e5371
Dony OMG <dony.develop@gmail.com>
2024-11-17 17:03:29 +0700

    initialize Vite + React + TypeScript project with Tailwind CSS setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import QRGeneratorForm from "@/components/qr-generator-form"
+
+
+function App() {
+
+    return (
+        <div>
+            <QRGeneratorForm />
+        </div>
+    )
+}
+
+export default App
